b94038773f9fa52565105643aee29a8d01895220
Diego Andrés Ramírez Aragón <lowfill@gmail.com>
2016-09-29 11:26:54 +0200

    Refactor - Reorganización del código para hacerlo más reusable - Se hizo un cache de la información de los devices para hacer la carga más rápida - Se agregó la hoja de estilos

--- estaciones/js/liquen.js
+++ estaciones/js/liquen.js
@@ -3,45 +3,95 @@ $(document).ready(function() {
   var devices = L.markerClusterGroup();
   var countries = [];
   var cities = ['Madrid', 'London', 'Santiago'];
-  var chart = nv.models.lineChart();
-  var chartData;
+  var machineSensor = nv.models.lineChart(),
+      humanSensor = nv.models.lineChart();
+  var machineData,humanData;
+  var tickFormats = {
+    "year":"%Y",
+    "month": "%b",
+    "week": "%x",
+    "day": "%x",
+    "hour": "%X",
+  }
+
+  function timeTickFormat(d){
+    zoom = 'day';
+    return d3.time.format(tickFormats[zoom])(new Date(d));
+  }
+
+  function noiseTickFormat(d){
+    if (d == null) {
+        return 'N/A';
+    }
+    return d3.format(',.2f')(d);
+  }
+
+  function loadNoice(device){
+    //TODO Limit by date
+    console.log(device);
+    $.getJSON('https://api.smartcitizen.me/v0/devices/'+device+'/readings?all_intervals=true&from=2016-09-20T14:14:44.889Z&rollup=12h&sensor_id=7&to=2016-09-27T14:14:44.890Z', function(resp) {
+      console.log(resp.readings);
+      var points = resp.readings.map(function(d){
+        ts = new Date(d[0]);
+        value = (d[1]!=null)?d[1]:0;
+        return {x:ts.getTime(),y:value};
+      })
+      var serie = {
+        key:"Noise",
+        values:points
+      }
+      console.log(points);
+      machineData
+              .datum([serie])
+              .transition().duration(1200)
+              .call(machineSensor);
+
+
+     nv.utils.windowResize(machineSensor.update);
+   });
+  }
+  nv.addGraph(function(){
+    machineSensor.options({duration: 300,useInteractiveGuideline: true});
+    machineSensor.xAxis.scale(d3.time.scale());
+
+    machineSensor.xAxis
+        .axisLabel("Time (s)")
+        .tickFormat(timeTickFormat)
+        .staggerLabels(true);
+
+    machineSensor.yAxis
+        .axisLabel('Noise (db)')
+        .tickFormat(noiseTickFormat);
+
+    machineData = d3.select('#machine-sensor > svg');
+    machineData
+            .datum([])
+            .transition().duration(1200)
+            .call(machineSensor);
+
+    return machineSensor;
+  });
 
   nv.addGraph(function(){
-    chart.options({duration: 300,
-            useInteractiveGuideline: true});
-    // chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return themselves, not the parent chart, so need to chain separately
-    chart.xAxis.scale(d3.time.scale());
-    var tickFormats = {
-      "year":"%Y",
-      "month": "%b",
-      "week": "%x",
-      "day": "%x",
-      "hour": "%X",
-    }
+    humanSensor.options({duration: 300,useInteractiveGuideline: true});
+    humanSensor.xAxis.scale(d3.time.scale());
 
-    chart.xAxis
+    humanSensor.xAxis
         .axisLabel("Time (s)")
+        .tickFormat(timeTickFormat)
+        .staggerLabels(true);
 
-        .tickFormat(function(d){
-          zoom = 'day';
-          return d3.time.format(tickFormats[zoom])(new Date(d));
-        })
-        .staggerLabels(true)
-    ;
-
-    chart.yAxis
+    humanSensor.yAxis
         .axisLabel('Noise (db)')
-        .tickFormat(function(d) {
-            if (d == null) {
-                return 'N/A';
-            }
-            return d3.format(',.2f')(d);
-        })
-    ;
+        .tickFormat(noiseTickFormat);
 
-    chartData = d3.select('#graph').append('svg');
+    humanData = d3.select('#human-sensor svg');
+    humanData
+            .datum([])
+            .transition().duration(1200)
+            .call(humanSensor);
 
-    return chart;
+    return humanSensor;
   });
 
   var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
@@ -76,36 +126,10 @@ $(document).ready(function() {
 
   $(document).on('click','.details',function(e){
     var device = $(e.target).data('device');
-    //TODO Limit by date
-    $.getJSON('https://api.smartcitizen.me/v0/devices/'+device+'/readings?all_intervals=true&from=2016-09-20T14:14:44.889Z&rollup=12h&sensor_id=7&to=2016-09-27T14:14:44.890Z', function(resp) {
-      console.log(resp.readings);
-      var points = resp.readings.map(function(d){
-        console.log(d);
-        ts = new Date(d[0]);
-        value = (d[1]!=null)?d[1]:0;
-        return {x:ts.getTime(),y:value};
-      })
-      var serie = {
-        key:"Noise",
-        values:points
-      }
-      console.log(points);
-      chartData
-              .datum([serie])
-              .transition().duration(1200)
-              .call(chart);
-
-
-  nv.utils.windowResize(chart.update);
-
-    });
+    loadNoice(device);
   });
 
-  $.getJSON('https://api.smartcitizen.me/v0/devices/world_map', function(resp) {
-    data = resp.filter(function(d) {
-      return d.system_tags.indexOf('outdoor') >= 0 &&
-        (countries.indexOf(d.country_code) >= 0 || cities.indexOf(d.city) >= 0);
-    });
+  $.getJSON('data/devices.json', function(data) {
     data.forEach(function(d) {
       var template = '<h2>{title}</h2><p>{description}</p><input class="details" data-device="{device}" type="button" value="View">';
       var popup = template.replace('{title}', d.name);
@@ -113,11 +137,8 @@ $(document).ready(function() {
       popup = popup.replace('{description}', d.description);
       popup = popup.replace('{device}', d.id);
 
-      //https://api.smartcitizen.me/v0/devices/2545
-
       L.marker([d.latitude, d.longitude]).bindPopup(popup).addTo(devices);
     });
-    console.log(data);
   })
 
 });
