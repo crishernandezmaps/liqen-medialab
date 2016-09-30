9c6bd76b7f65690f3c820716b40ad011db583f2e
Carlos Saito <c.saito@alumnos.upm.es>
2016-09-30 14:08:59 +0200

    Move backend to "/backend" and modularize logic/data

---
+++ backend/index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongodb = require('mongodb');
+const calculateScore = require('./lib/logic')
+const db = require('./lib/db')
+
+const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('port', process.env.PORT || 5000)
+app.use(express.static('public'))
+
+// "index.html"
+app.get('/metrics', function (req, res) {
+  const sensor = req.body.device
+  const start  = Date.parse(req.body.start)
+  const end    = Date.parse(req.body.end)
+  db.read({ sensor, start, end})
+    .then(function (docs) {
+      res.json(docs)
+    })
+})
+
+// Get the form sending
+app.post('/metrics', function (req, res) {
+  const answers = {
+    'procedence' : req.body['procedence'],
+    'noise-disturbance' : req.body['noise-disturbance'],
+    'source' : req.body['source'],
+    'noisy-area' : req.body['noisy-area'],
+    'sleep-quality' : req.body['sleep-quality'],
+    'for-working-here' : req.body['for-working-here'],
+    'at-home' : req.body['at-home'],
+    'continuous' : req.body['continuous'],
+    'for-living-here' : req.body['for-living-here'],
+    'other-time' : req.body['other-time'],
+    'thirty-mins' : req.body['thirty-mins']
+  }
+
+  const r = calculateScore(answers)
+  const decibels = r.decibels
+  const score = r.score
+
+  const result = {
+    timestamp: Date.now(),
+    sensor: req.body.sensor_id,
+    position: {
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+    },
+    answers,
+    score,
+    decibels
+  }
+
+  // Now save "result" in MONGODB
+  // And redirect...
+  db.write(result)
+    .then(function (result) {
+      res.redirect('http://liqenproject.org')
+    })
+})
+
+app.listen(app.get('port'), function () {
+  console.log('Connected to database: ', process.env.MONGODB_URI)
+  console.log('Listening to port', app.get('port'))
+})
